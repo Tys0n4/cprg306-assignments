e6c304dd413c75985aca5e960c45a03fdb2a056e
Tys0n4 <thaiinn3@gmail.com>
2024-09-12 12:41:30 -0600

    Prepared Week 2 Assignment
    I Cleaned up the home page and I created the week 2 assignment page.

---
+++ app/week-2/page.js
@@ -0,0 +1,9 @@
+import StudentInfo from "./student-info";
+export default function Page() {
+    return (
+      <main>
+        <h1>CPRG306: Web Development 2 - Assignments</h1>
+        <StudentInfo/>
+      </main>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Thai Nguyen</h1>
+      <p>Check out my GitHub repository</p>
+      <Link href="https://github.com/Tys0n4/cprg306-assignments.git">My GitHub</Link>
+    </div>
+  );
+};
+
+export default StudentInfo;
