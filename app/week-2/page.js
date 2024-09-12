352add2ee01c6ed2212be9954eee4c655861846f
Tys0n4 <thaiinn3@gmail.com>
2024-09-12 13:06:20 -0600

    Finishing touches on the assignments
    A few changes for the assignment.

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,10 @@
 import StudentInfo from "./student-info";
+
 export default function Page() {
-    return (
-      <main>
-        <h1>CPRG306: Web Development 2 - Assignments</h1>
-        <StudentInfo/>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo/>
+    </main>
+  );
+}
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,8 +4,7 @@ const StudentInfo = () => {
   return (
     <div>
       <h1>Thai Nguyen</h1>
-      <p>Check out my GitHub repository</p>
-      <Link href="https://github.com/Tys0n4/cprg306-assignments.git">My GitHub</Link>
+      <Link href="https://github.com/Tys0n4/cprg306-assignments.git">https://github.com/Tys0n4/cprg306-assignments.git</Link>
     </div>
   );
 };
